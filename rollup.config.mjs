fda1f8bf2b7cc79cc3a5f3d4d151b78bc12c7c6c
Frederid Palacios <frederidp@gmail.com>
2023-10-15 21:11:19 -0600

    Removing dependency on JSON resources

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,11 @@
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json' assert {type: "json"};
 
 export default {
     input: 'src/erebus.mjs',
     output: {
-        file: pkg.browser,
+        file: './dist/erebus-core.min.js',
         format: 'umd',
         name: 'Erebus',
         sourcemap: true
